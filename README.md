a81ab6dde65780267ef13af724070d597e94b261
Kashif Saadat <kashifsaadat@gmail.com>
2018-08-16 18:30:21 +0100

    General cleanup of the image and backup script

--- README.md
+++ README.md
@@ -1,26 +1,31 @@
-# docker-etcd-backup
+# Docker Image: ETCD Backup
 
-[![Build Status](https://drone.digital.homeoffice.gov.uk/api/badges/UKHomeOffice/docker-etcd-backup/status.svg)](https://drone.digital.homeoffice.gov.uk/UKHomeOffice/docker-etcd-backup)
+[![Build Status](https://drone.acp.homeoffice.gov.uk/api/badges/UKHomeOffice/docker-etcd-backup/status.svg)](https://drone.acp.homeoffice.gov.uk/UKHomeOffice/docker-etcd-backup)
 
-Cluster Backup process for etcd3
+Cluster Backup process for ETCD v3
 
 ## Cluster backup
 
 A cluster backup is made with `etcdctl backup` which strips node information thus requiring a cluster rebuild. This backup is saved as a tar file in the `/tmp` directory by default. If `S3_PATH` is specified it will be uploaded to S3 and encrypted with the KMS key that you have specified using the `KMS_ID` variable.
 
-## Environment Variables
+## Configurable options
 
-* `BACKUP_PATH:-/tmp` The root directory to backup to
-* `ETCD_ENDPOINT:-https://localhost:4001`
-* `S3_PATH` Will backup to this path and delete off host when done
-* `KMS_ID` Specifies which KMS encryption key to use in S3
-* `CA_FILE:-/srv/kubernetes/ca.crt` Path to the CA for your etcd cluster
-* `ETCDCTL_CERT` Path to the cert for your etcd cluster
-* `ETCDCTL_KEY` Path to the private key for your etcd cert
+The following environment variables can be passed in to configure etcd backups for your environment:
+
+| ENVIRONMENT VARIABLE | DESCRIPTION | REQUIRED | DEFAULT VALUE |
+|----------------------|-------------|----------|---------------|
+| BACKUP_PATH | Set the directory to copy the etcd backup to | N | `/tmp` |
+| ETCDCTL_CACERT | Verify the ETCD certificate using this CA bundle | N | `/srv/kubernetes/ca.crt` |
+| ETCDCTL_CERT | TLS certificate file for ETCD | N | NULL |
+| ETCDCTL_ENDPOINT | ETCD endpoint | N | `https://localhost:2379` |
+| ETCDCTL_KEY | TLS key file for ETCD | N | NULL |
+| S3_AWS_ENDPOINT | Custom S3 endpoint URL | N | NULL |
+| S3_CA_BUNDLE | The CA bundle for the S3 endpoint | N | NULL |
+| S3_KMS_ID | A KMS ID to use for encrypting the backups in S3 | N | NULL |
+| S3_NO_SSL_VERIFY | Skip TLS verify for the S3 endpoint | N | `false` |
+| S3_PATH | Provide the S3 bucket path to copy the backup to, e.g. `s3://my-bucket`<br>If unset, the backup is left in `BACKUP_PATH` | N | NULL |
 
 ## Restore process
 
-### From Cluster Backup
-
 1. Stop ETCD everywhere
-2. See: https://github.com/coreos/etcd/blob/master/Documentation/v2/admin_guide.md#disaster-recovery
+2. See: https://github.com/coreos/etcd/blob/master/Documentation/op-guide/recovery.md
--- bin/backup.sh
+++ bin/backup.sh
@@ -5,51 +5,52 @@ set -o pipefail
 
 [[ ${DEBUG} == 'true' ]] && set -x
 
-# Use the v3 api
+export NC='\e[0m'
+export GREEN='\e[0;32m'
+export YELLOW='\e[0;33m'
+export RED='\e[0;31m'
+
+log()     { (2>/dev/null echo -e "$@${NC}"); }
+info()    { log "${GREEN}[INFO] $@"; }
+warning() { log "${YELLOW}[WARNING] $@"; }
+error()   { log "${RED}[ERROR] $@"; }
+
+export DATESTAMP=`date +%Y%m%d_%H%M`
+export BACKUP_PATH=${BACKUP_PATH:-/tmp}
+export BACKUP_FILE=${BACKUP_PATH}/etcd_backup.db
+export BACKUP_TAR=${BACKUP_PATH}/etcd_${DATESTAMP}.tar.gz
 export ETCDCTL_API=3
-
-ETCD_ENDPOINT=${ETCD_ENDPOINT:-https://localhost:4001}
-CA_FILE=${CA_FILE:-/srv/kubernetes/ca.crt}
-ETCD_CMD=${ETCD_CMD:-"etcdctl --cacert ${CA_FILE}"}
-DATESTAMP=`date +%Y%m%d_%H%M`
-BACKUP_PATH=${BACKUP_PATH:-/tmp}
-BACKUP_FILE=${BACKUP_PATH}/etcd_backup.db
-BACKUP_TAR=${BACKUP_PATH}/etcd_${DATESTAMP}.tar.gz
-ETCD_ENDPOINTS=`${ETCD_CMD} --endpoints ${ETCD_ENDPOINT} member list | awk '{print $5}' | tr '\n' ',' | sed s/.$//`
-
-function error_exit() {
-  echo "ERROR: ${1}"
-  exit 1
-}
-
-function info() {
-  echo "INFO: ${1}"
-}
+export ETCDCTL_CACERT=${ETCDCTL_CACERT:-/srv/kubernetes/ca.crt}
+export ETCDCTL_ENDPOINTS=${ETCDCTL_ENDPOINTS:-https://localhost:2379}
+export ETCDCTL_ENDPOINTS=`etcdctl member list | awk '{print $5}' | tr '\n' ',' | sed s/.$//`
 
 function move_s3() {
+  # No-op when no backup
+  if [[ -z ${S3_PATH} ]]; then
+    warning "No 'S3_PATH' has been defined, backup is located in ${BACKUP_TAR}"
+    exit 0
+  fi
+
   local destpath=${S3_PATH}${BACKUP_TAR#${BACKUP_PATH}}
   local s3cli_args=""
-  # Noop when no backup
-  [[ -z ${S3_PATH} ]] && \
-    return 0
 
   # Check destination before uploading
-  if [[ -f ${BACKUP_TAR} ]] ; then
+  if [[ -f ${BACKUP_TAR} ]]; then
     info "Uploading backed up file to s3"
-    if [[ ${CA_BUNDLE} ]] ; then
-      s3cli_args+=" --ca-bundle ${CA_BUNDLE}"
-    elif [[ ${NO_SSL_VERIFY} ]]; then
+    if [[ -n ${S3_CA_BUNDLE} ]]; then
+      s3cli_args+=" --ca-bundle ${S3_CA_BUNDLE}"
+    elif [[ ${S3_NO_SSL_VERIFY} == true ]]; then
       s3cli_args+=" --no-verify-ssl"
     fi
-    if [[ -n ${AWS_ENDPOINT} ]] ; then
-      s3cli_args+=" --endpoint-url ${AWS_ENDPOINT}"
+    if [[ -n ${S3_AWS_ENDPOINT} ]]; then
+      s3cli_args+=" --endpoint-url ${S3_AWS_ENDPOINT}"
     fi
-    if [[ -n ${KMS_ID} ]] ; then
-      s3cli_args+=" --sse aws:kms --sse-kms-key-id ${KMS_ID}"
+    if [[ -n ${S3_KMS_ID} ]]; then
+      s3cli_args+=" --sse aws:kms --sse-kms-key-id ${S3_KMS_ID}"
     fi
-      aws s3 mv ${s3cli_args} ${BACKUP_TAR} ${destpath}
+    aws s3 mv ${s3cli_args} ${BACKUP_TAR} ${destpath}
   else
-    error_exit "Backed up file does not exist"
+    error "Backed up file does not exist"
     exit 1
   fi
 }
@@ -60,18 +61,17 @@ function tar_backup() {
     tar -cvzf ${BACKUP_TAR} ${BACKUP_FILE}
     rm -fr ${BACKUP_FILE}
   )
-  info "Backed up to:${BACKUP_FILE}"
+  info "Backed up to: ${BACKUP_TAR}"
 }
 
 # Creates a backup of the current cluster
 function clusterbackup() {
-
   info "Start Cluster Backup"
-  ${ETCD_CMD} --endpoints ${ETCD_ENDPOINTS} snapshot save ${BACKUP_FILE}
+  etcdctl snapshot save ${BACKUP_FILE}
   tar_backup
   move_s3
 }
 
-[[ -z ${ETCD_ENDPOINTS} ]] && error_exit "Could not find etcd endpoints"
+[[ -z ${ETCDCTL_ENDPOINTS} ]] && error "Could not find etcd endpoints" && exit 1
 
 clusterbackup
